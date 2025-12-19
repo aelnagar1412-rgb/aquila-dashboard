f422fbc13149a0ad56f854888ddb52805cc2bfdb
aelnagar1412-rgb <aelnagar1412@gmail.com>
2025-12-20 01:42:29 +0200

    Update strategy.py

--- engine/strategy.py
+++ engine/strategy.py
@@ -1,19 +1,22 @@
-import random
+def rsi_ema_strategy(rsi, price, ema50, candle):
+    if rsi < 30 and price > ema50 and candle == "green":
+        return "CALL"
+    if rsi > 70 and price < ema50 and candle == "red":
+        return "PUT"
+    return None
 
-def analyze_market(pair, timeframe):
-    """
-    Dummy strategy (مرحلة أولى)
-    هتتبدل بتحليل حقيقي بعد شوية
-    """
 
-    # محاكاة إشارة
-    signal = random.choice(["BUY", "SELL", None])
+def trend_pullback_strategy(ema20, ema50, rsi):
+    if ema20 > ema50 and 40 <= rsi <= 50:
+        return "CALL"
+    if ema20 < ema50 and 50 <= rsi <= 60:
+        return "PUT"
+    return None
 
-    if signal:
-        return {
-            "pair": pair,
-            "timeframe": timeframe,
-            "signal": signal
-        }
 
+def breakout_strategy(price, high10, low10, rsi, volume, avg_volume):
+    if price > high10 and rsi > 55 and volume > avg_volume:
+        return "CALL"
+    if price < low10 and rsi < 45 and volume > avg_volume:
+        return "PUT"
     return None
